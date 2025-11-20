e7bf42fbfb59cb9721a50d655045c25f9b1d460d
clement <clement.augoyard@gmail.com>
2025-11-20 17:54:05 +0100

    ramassable

--- anticairshop/Assets/Dialogue/Dialogue System/Scripts/PickableItem.cs
+++ anticairshop/Assets/Dialogue/Dialogue System/Scripts/PickableItem.cs
@@ -12,9 +12,10 @@ public class PickableItem : MonoBehaviour
     public int requiredItemCount = 0;
 
     [Header("UI Messages")]
-    public string availableMessage = "Ramasser l'objet";
+    public string availableMessage = "examiner l'objet";
+    public string firstMessage = "tu examine l'objet";
     public string missingRequirementMessage = "Un autre objet est nécessaire";
-    public string alreadyPickedMessage = "Cet objet a déjà été ramassé";
+    public string alreadyPickedMessage = "Cet objet a déjà été examiné";
 
     [Header("Visuals")]
     public Color targetColor = Color.yellow;
@@ -29,7 +30,6 @@ public class PickableItem : MonoBehaviour
     private bool over = false;
     private bool hasBeenPicked = false;
     private bool canInteract = true; // ✅ Nouveau flag
-
     private Interact interactUI;
     private GameObject mainCam;
     private Coroutine hideCoroutine;
@@ -43,6 +43,7 @@ public class PickableItem : MonoBehaviour
         interactUI = mainCam.GetComponent<Interact>();
         rend = GetComponent<Renderer>();
         originColor = rend.material.color;
+        
 
         if (uiPanel != null)
             uiPanel.SetActive(false);
@@ -98,12 +99,12 @@ public class PickableItem : MonoBehaviour
                     DialogueManager.collectedItems++;
                     hasBeenPicked = true;
                     uiPanel.SetActive(true);
-                    ShowUI("Objet ramassé : " + itemName + " | Total = " + DialogueManager.collectedItems, 2f);
+                    ShowUI(firstMessage, 2f);
                 }
                 else
                 {
                     uiPanel.SetActive(true);
-                    ShowUI("Pas assez d’objets pour ramasser " + itemName, 2f);
+                    ShowUI(missingRequirementMessage, 2f);
                 }
             }
             else
